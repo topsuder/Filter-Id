94d178e56a8b6a9f36b20978bf82191e5b5f27bf
topsuder <suders@vip.qq.com>
2023-04-20 11:26:38 +0800

    init commit

--- src/main/java/fun/suder/FilterIDTest.java
+++ src/main/java/fun/suder/FilterIDTest.java
@@ -1,4 +1,8 @@
-package fun.suder;/**
+package fun.suder;
+
+import java.util.stream.Stream;
+
+/**
  * <span>Form File</span>
  * <p>Description</p>
  * <p>Copyright: Copyright (c) 2022 版权</p>
@@ -9,5 +13,17 @@ package fun.suder;/**
  * @DATE 2023/4/20-10:45
  * @Description
  * @see fun.suder Filter-Id
- */public class FilterIDTest {
+ */
+public class FilterIDTest {
+
+    public static void main(String[] args) {
+        System.out.println(filterId("4301242260039"));
+    }
+
+    public static boolean filterId(String id) {
+        String filterId = "4301242260039-1A---00----3/0----2A-------01";
+        return Stream.of(filterId.split("/"))
+                .anyMatch(data->data.substring(0, filterId.indexOf("-")).equals(id));
+    }
+
 }
